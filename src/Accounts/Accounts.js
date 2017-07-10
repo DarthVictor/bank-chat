15486b645be6ddfdbb51254fdd37315d8bda0794
Victor Follet <follet.victor@gmail.com>
2017-07-11 02:02:01 +0300

    Some implementation of accounts__tab-selector

--- src/Accounts/Accounts.js
+++ src/Accounts/Accounts.js
@@ -4,11 +4,14 @@ import './Accounts.scss'
 import AccountList from './AccountList'
 // import DepoList from './DepoList'
 
+const ACCOUNTS_TEXT = 'Счета'
+const DEPO_TEXT = 'Вклады'
 export default class Accounts extends React.Component {
   render() {
     return <div className="accounts">
         <div className="accounts__tab-selector">
-
+          <button  className="accounts__tab-button is-active">{ACCOUNTS_TEXT}</button>
+          <button  className="accounts__tab-button">{DEPO_TEXT}</button>
         </div>
         <AccountList></AccountList>
         {/*<DepoList></DepoList>*/}
